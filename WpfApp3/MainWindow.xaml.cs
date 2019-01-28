ff4461b06228945fbded60b5260f66a3bd8261a4
losde5530 <losde5530@gmail.com>
2019-01-28 21:46:53 +0300

    Project 3. Version 2

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp3
 {
@@ -20,9 +13,85 @@ namespace WpfApp3
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static int localPort = 8080;
+        public Task task;
+        public CancellationTokenSource cancelTokenSource;
+        public CancellationToken token;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// По клику на Старт запускаем поток, принимающий данные по UDP
+        /// </summary>
+        private void ButtonClickStart(object sender, RoutedEventArgs e)
+        {
+            cancelTokenSource = new CancellationTokenSource();
+            token = cancelTokenSource.Token;
+            task = new Task(
+                () => ReceiveMessage(token)
+            );
+            task.Start();
+        }
+
+        /// <summary>
+        /// Завершаем поток по клику на Стоп
+        /// </summary>
+        private void ButtonClickStop(object sender, RoutedEventArgs e)
+        {
+            cancelTokenSource.Cancel();
+            ChangeTextBlock("0");
+        }
+
+        /// <summary>
+        /// Прослушка порта
+        /// </summary>
+        private void ReceiveMessage(CancellationToken token)
+        {
+            UdpClient receiver = new UdpClient(localPort);
+            IPEndPoint remoteIp = null;
+            try
+            {
+                while (true)
+                {
+                    byte[] data = receiver.Receive(ref remoteIp);
+                    string message = Encoding.Unicode.GetString(data);
+                    if (token.IsCancellationRequested)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        ChangeTextBlock(message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                receiver.Close();
+            }
+        }
+
+        /// <summary>
+        /// Изменение значения индикатора
+        /// </summary>
+        private void ChangeTextBlock(string message)
+        {
+            textBlock.Dispatcher.BeginInvoke(
+                new Action(
+                    delegate ()
+                    {
+                        textBlock.Text = message;
+
+                    }
+                )
+            );
         }
     }
 }
